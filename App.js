ac63d442892e1b4a781d754b283135f7fe225c4d
hungvu <hungns126@gmail.com>
2025-03-06 17:51:55 +0700

    init

---
+++ App.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <View style={{backgroundColor: 'green', padding: 20}}>
+      <Text>App aaaa</Text>
+    </View>
+  );
+}
